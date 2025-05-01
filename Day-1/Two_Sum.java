c319dd61c14b18139500a532bd50aad1fb0022f1
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-05-01 21:29:51 +0530

    Day 1 | 1. Two Sum | JAVA

---
+++ Day-1/Two_Sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i<nums.length; i++)
+        {
+            for(int j = i+1; j<nums.length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                     return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+
+class Two_Sum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        int[] result = solution.twoSum(nums, target);
+        
+        // Print the result
+        System.out.print("[");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i < result.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
